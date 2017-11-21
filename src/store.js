b17fbaed5dcb9e0926c4579c4199a1aefc962802
jais210 <jackelineisabel.puruaya@gmail.com>
2017-11-21 05:30:44 -0500

    store

--- src/store.js
+++ src/store.js
@@ -1,2 +1,22 @@
 
+import createStore from 'redux-zero'
+// identifico los datos a inicializar en mi store
+const initialState = {
+   successLogin : false,
+
+   user : {
+      id : null,
+      email :  null,
+      fullname :  null,
+      survey :  null,
+      question :  null,
+      options :  null            
+   },
+   boards : null,
+   stages : null,
+   tasks : null   
+}
+
+const store = createStore (initialState);
+export default store
 
