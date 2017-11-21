77b7aae107db91596a59f46e100d527620ba929d
jais210 <jackelineisabel.puruaya@gmail.com>
2017-11-21 05:33:53 -0500

    actions remodelado

--- src/actions.js
+++ src/actions.js
@@ -1,19 +1,5 @@
 import store from './store';
-import firebase from 'firebase';
 import {auth, database} from './firebase';
-
-// Initialize Firebase
-// var config = {
-//     apiKey: "AIzaSyD2QTBGifnDuVlIPhTD8uhoI2cqtWrcm4Q",
-//     authDomain: "trello-88f82.firebaseapp.com",
-//     databaseURL: "https://trello-88f82.firebaseio.com",
-//     projectId: "trello-88f82",
-//     storageBucket: "trello-88f82.appspot.com",
-//     messagingSenderId: "242725123531"
-//   };
-//   firebase.initializeApp(config);
-
-  
 
 export function readBoard(user) {
     firebase.database().ref(user + '/boards').on('value', res => {
