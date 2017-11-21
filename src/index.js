f8d467b7a5291117bfb984fac18e113612d75397
jais210 <jackelineisabel.puruaya@gmail.com>
2017-11-21 06:07:19 -0500

    index change

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Trello from './Trello';
-import registerServiceWorker from './registerServiceWorker';
-import { Provider } from "redux-zero/react";
-import store from "./store";
-import {probando} from './actions';
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import Trello from './Trello';
+// import registerServiceWorker from './registerServiceWorker';
+// import { Provider } from "redux-zero/react";
+// import store from "./store";
+// import {probando} from './actions';
 
-const Index = () => (
-	<Provider store={store}>
-		<Trello />
-	</Provider>
-);
+// const Index = () => (
+// 	<Provider store={store}>
+// 		<Trello />
+// 	</Provider>
+// );
 
-ReactDOM.render(<Index />, document.getElementById('root'));
-registerServiceWorker();
+// ReactDOM.render(<Index />, document.getElementById('root'));
+// registerServiceWorker();
+
